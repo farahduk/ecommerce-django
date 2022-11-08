3893693b96a45d3855fce7866fa6016cc242135b
farahduk <farahduk@gmail.com>
2022-11-08 01:37:41 -0500

    tienda funcionando con variants, pero sin resolver el inventario

--- store/models.py
+++ store/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    Created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
     def get_url(self):
@@ -20,3 +20,30 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+    
+    def tallas(self):
+        return super(VariationManager, self).filter(variation_category='talla', is_active=True)
+
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('talla', 'talla')
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateField(auto_now=True)
+    objects = VariationManager()
+    def __str__(self):
+        return self.variation_category + ':' + self.variation_value
+
+
+
